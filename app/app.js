09547af05eebfffb0cca381e7223849eeb447310
heyipan1 <heyipan1@sina.com>
2018-08-23 23:24:08 +0800

    导航焦点状态切换

--- app/app.js
+++ app/app.js
@@ -4,11 +4,21 @@
 	* 主模块
 	*
 	* */
-	var moviecat = angular.module('movieCat', [
-		'ngRoute',
-		'movieCat.movie_list',
-	]);
-	moviecat.config(['$routeProvider', function($routeProvider) {
+	var movieCat = angular.module('movieCat', ['ngRoute','movieCat.movie_list']);
+	movieCat.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.otherwise({redirectTo: '/in_theaters/1'});
 	}]);
+
+	movieCat.controller("toggleController",['$scope','$location',function ($scope,$location) {
+		$scope.$location = $location;
+		$scope.$watch("$location.path()",function (now,old) {
+			if (now.indexOf('in_theaters') != -1){
+				$scope.toggle = "in_theaters";
+			}else if (now.indexOf('coming_soon') != -1){
+				$scope.toggle = "coming_soon";
+			}else if (now.indexOf("top250") != -1 ){
+				$scope.toggle = "top250";
+			}
+		})
+	}])
 })(angular);
