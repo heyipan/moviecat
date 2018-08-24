b6b176fd6e75722cd63bf1ddb2042ee7c071774f
heyipan1 <heyipan1@sina.com>
2018-08-24 23:44:57 +0800

    auto-foucus问题解决  jsonp回掉

--- app/components/autoFocus.js
+++ app/components/autoFocus.js
@@ -7,16 +7,15 @@
 		var path = $location.path();
 		return {
 			restrict:'A',
-			link:function (scope,elem,attr,controllers) {
+			link:function ($scope,elem,attr,controllers) {
+				$scope.$location = $location;
 				var aLink = elem.children().attr("href").split("/")[1];
-				if (path.indexOf(aLink) != -1){
-					console.log("11")
-					elem.addClass("active");
-				}
-				elem.on("click",function () {
-					elem.parent().children().removeClass("active");
-					elem.addClass("active");
-				})
+				$scope.$watch('$location.path()',function (now) {
+					if (now.indexOf(aLink) != -1){
+						elem.parent().children().removeClass("active");
+						elem.addClass("active");
+					}
+				});
 			}
 		}
 	}])
--- app/components/http.js
+++ app/components/http.js
@@ -8,7 +8,6 @@
 			var fnSuffix = Math.random().toString().replace('.', '');
 			var cbFuncName = 'my_json_cb_' + fnSuffix;
 			// 不推荐 在angular中 $window 就是 window 而$document[0] 对象就是document对象
-			$window[cbFuncName] = callback;
 			var querystring = url.indexOf('?') == -1 ? '?' : '&';
 			for (var key in data) {
 				querystring += key + '=' + data[key] + '&';
@@ -16,6 +15,11 @@
 			querystring += 'callback=' + cbFuncName;
 			var scriptElement = $document[0].createElement('script');
 			scriptElement.src = url + querystring;
+			$window[cbFuncName] = function (data) {
+				callback(data);
+				//清除请求标签
+				document.body.removeChild(scriptElement);
+			};
 			document.body.appendChild(scriptElement);
 		};
 	}])
